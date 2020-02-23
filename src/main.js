e909933c515f60ac0e9585edd5acdec9071ce0a8
Brandan Sayarath <brandan@brandan.tech>
2020-02-23 10:06:24 -0800

    refactor code to display HTTP error code if status is not 200

--- src/main.js
+++ src/main.js
@@ -7,30 +7,31 @@ import { Doctors } from './better-doctor';
 // Doctor Search Function
 async function doctorSearch(input){
 
-  let doctor = new Doctors (input);
+  let doctor = new Doctors(input);
+
 
   await doctor.getDoctor(input);
 
-  if (doctor.status === false) {
-    $('.errors').append('Error! Status not found');
-  }
-  if( doctor.input == '') {
+  if (doctor.input) {
+    await getDoctors(doctor.jsonifiedResponse);
+  } else {
     $('.errors').append('Error! Please input a name!');
   } 
-  await getDoctors(doctor.jsonifiedResponse);
 }
-  
+
 function getDoctors(doctor) {
   console.log(doctor);
 
-  if (doctor.data == 0) {
+  if (!doctor.data) {
+    $('.errors').append(`Error! The API is returning the following status: ${doctor.status} ${doctor.statusText}.`);
+  } else if (doctor.data == 0) {
     $('#showdocsname').html("We did not find any doctors matching your name.");
-  } 
-  else { 
+  } else { 
     for (var i = 0; i < doctor.data.length; i++)
       $('#showdocsname').append(` <li> ${doctor.data[i].profile.first_name} ${doctor.data[i].profile.last_name}<br>${doctor.data[i].practices[0].name} <br> Address: ${doctor.data[i].practices[0].visit_address.street}, ${doctor.data[i].practices[0].visit_address.city}, ${doctor.data[i].practices[0].visit_address.state} ${doctor.data[i].practices[0].visit_address.zip} <br> Phone number: ${doctor.data[i].practices[0].phones[0].number} <br> Accepting new patients: ${doctor.data[i].practices[0].accepts_new_patients} </li>` );
   } 
 }
+
 
 // Symptom Search Function
 async function symptomSearch(input){
@@ -39,30 +40,29 @@ async function symptomSearch(input){
 
   await symptom.getSymptom(input);
 
-  if (symptom.status === false) {
-    $('.errors').append('Error! Status not found');
-  }
-  if( symptom.input === '') {
-    $('.errors').append('Error! Please input a name!');
+  if (symptom.input) {
+    await getSymptoms(symptom.jsonifiedResponse);
+  } else {
+    $('.errors').append('Error! Please input a symptom!');
   } 
-  await getSymptoms(symptom.jsonifiedResponse);
+  
+  if (symptom.jsonifiedResponse === false) {
+    $('.errors').append('Error! Cannot make the API call!');
+  }
 }
 
 function getSymptoms(symptom) {
   console.log(symptom);
 
-  if (symptom.data == 0) {
+  if (!symptom.data) {
+    $('.errors').append(`Error! The API is returning the following status: ${symptom.status} ${symptom.statusText}.`);
+  } else if (symptom.data == 0) {
     $('#showdocssymptom').html("Sorry. We did not find any doctors that treat your symptoms.");
-  } 
-  else { 
+  } else { 
     for (var i = 0; i < symptom.data.length; i++)
       $('#showdocssymptom').append(` <li> ${symptom.data[i].profile.first_name} ${symptom.data[i].profile.last_name}<br>${symptom.data[i].practices[0].name} <br> Address: ${symptom.data[i].practices[0].visit_address.street}, ${symptom.data[i].practices[0].visit_address.city}, ${symptom.data[i].practices[0].visit_address.state} ${symptom.data[i].practices[0].visit_address.zip} <br> Phone number: ${symptom.data[i].practices[0].phones[0].number} <br> Accepting new patients: ${symptom.data[i].practices[0].accepts_new_patients} </li>` );
   }   
 }
-
-
-
-
 
 
 $(document).ready(function() {
